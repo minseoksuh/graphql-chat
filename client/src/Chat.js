2558f9531749e9faff6c71b6f1b7c85ffa8352bb
Minseok Suh <strangeloop89@gmail.com>
2021-04-11 14:49:48 +0900

    feat: add message subscription in client

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
-import { addMessage, getMessages } from './graphql/queries';
-import MessageInput from './MessageInput';
-import MessageList from './MessageList';
+import React, { Component } from "react";
+import { addMessage, getMessages, onMessageAdded } from "./graphql/queries";
+import MessageInput from "./MessageInput";
+import MessageList from "./MessageList";
 
 class Chat extends Component {
-  state = {messages: []};
+  state = { messages: [] };
+  subscription = null;
 
   async componentDidMount() {
     const messages = await getMessages();
-    this.setState({messages});
+    this.setState({ messages });
+
+    console.log("comdidmount");
+
+    this.subscription = await onMessageAdded((message) => {
+      this.setState({ messages: this.state.messages.concat(message) });
+    });
+
+    console.log(this.subscription);
+  }
+
+  componentWillUnmount() {
+    if (this.subscription) {
+      console.log(this.subscription);
+      this.subscription.unsubscribe();
+    }
   }
 
   async handleSend(text) {
-    const message = await addMessage(text);
-    this.setState({messages: this.state.messages.concat(message)});
+    await addMessage(text);
   }
 
   render() {
-    const {user} = this.props;
-    const {messages} = this.state;
+    const { user } = this.props;
+    const { messages } = this.state;
     return (
       <section className="section">
         <div className="container">
@@ -28,7 +43,7 @@ class Chat extends Component {
         </div>
       </section>
     );
-  }  
+  }
 }
 
 export default Chat;
